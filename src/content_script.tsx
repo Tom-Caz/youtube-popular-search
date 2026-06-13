91a18aa6d3a382b25e3002b969fc2786f9bf64ec
Thomas Cazort <thomas.cazort@gmail.com>
2026-06-13 14:50:51 -0700

    design: align with youtube

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -160,8 +160,11 @@ function enhancePopularChip(button: HTMLButtonElement): void {
 
   const caret = document.createElement("span");
   caret.className = "ytps-caret";
-  caret.textContent = "▾";
   caret.setAttribute("aria-hidden", "true");
+  caret.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false" aria-hidden="true">' +
+    '<path d="M18.707 8.793a1 1 0 00-1.414 0L12 14.086 6.707 8.793a1 1 0 10-1.414 1.414L12 16.914l6.707-6.707a1 1 0 000-1.414Z"></path>' +
+    "</svg>";
 
   labelContainer.appendChild(rangeSpan);
   labelContainer.appendChild(caret);
